59c76cb4c517bd7a539316c746d00ea181f18a43
Caio Nogueira <caio@fuerzastudio.com.br>
2022-09-07 01:50:41 -0300

    Adicionada nova página, Logistica.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,26 @@ import logoFooter from '../assets/logo_branca.png';
 
 function Footer() {
   return (
-    <div className="footer">
+    <div className="footer mt-5">
+      <div className="py-4 border-bottom border-top">
+        <div className="container grid__status">
+          <div className="status__item">
+            <h4>14</h4>
+            <p>Anos de experiência</p>
+          </div>
+          <div className="status__item">
+            <h4>1.200m²</h4><p>Para armazenamento</p>
+          </div>
+          <div className="status__item">
+            <h4>+2 Milhões</h4>
+            <p>De pedidos operados</p>
+          </div>
+          <div className="status__item">
+            <h4>+900</h4>
+            <p>Clientes</p>
+          </div>
+        </div>
+      </div>
       <div className="container">
         <div className="footer__top border-bottom py-5">
           <div className="acesso">
@@ -25,19 +44,19 @@ function Footer() {
           </div>
           <div className="redesSociais">
             <a href="#0">
-            <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
+              <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
             </a>
             <a href="#0">
-            <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"></path><polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"></polygon></svg>
+              <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"></path><polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"></polygon></svg>
             </a>
             <a href="#0">
-            <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
+              <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="30" width="30" xmlns="http://www.w3.org/2000/svg"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
             </a>
           </div>
           <div className="ondeEstamos">
             <p>Onde Estamos?</p>
             <a href="#0">
-            Av. Industrial, 1120 - Distrito Industrial, Governador Valadares (MG) CEP: 35040-610
+              Av. Industrial, 1120 - Distrito Industrial, Governador Valadares (MG) CEP: 35040-610
             </a>
           </div>
           <div className="sac">
@@ -52,7 +71,7 @@ function Footer() {
           </div>
         </div>
         <div className="footer__bottom py-4">
-          <img style={{width: "175px"}} src={logoFooter} alt="Logo Bottom" />
+          <img style={{ width: "175px" }} src={logoFooter} alt="Logo Bottom" />
           <span>Feito com ❤️ por <a href="https://www.swordagency.com.br">Sword Agency</a> </span>
         </div>
       </div>
--- src/Components/Logistica.jsx
+++ src/Components/Logistica.jsx
@@ -4,11 +4,203 @@ import { Banner } from './index';
 const Logistica = () => {
   return (
     <section>
-      <Banner name="teste" subtitle="teste de subtitle" />
+      <Banner name="Logística" 
+      subtitle=
+      "Através de uma logística desenvolvida com as melhores estratégias, cuidamos de toda parte d armazenamento e entrega dos seus produtos." />
       <div className="logistica">
-        <div className="container">
-          <h1 className="text-center mt-5">Página Logística</h1>
+        <div className="container position-relative">
+          <div className="logistica-content py-5">
+            <div className="container">
+              <div className="row align-items-center ">
+                <div className="col-lg-6 col-md-12">
+                  <h2>
+                    Nós levamos o seu produto até o seu cliente.
+                  </h2>
+                  <p>
+                    Com um centro de distribuição 
+                    localizado em Governador Valadares (MG),
+                    oferecemos uma infraestrutura de qualidade
+                    para estocar os seus itens. Assim, 
+                    você não precisa se preocupar com o 
+                    gerenciamento de estoque.
+                  </p>
+                  <p>
+                  Contamos também com soluções inteligentes
+                   para que o transporte seja realizado com 
+                   precisão e rapidez em todo território nacional. 
+                   Nosso time é composto por pessoas qualificadas 
+                   para que todo o processo seja otimizado e 
+                   feito de maneira eficiente.
+                  </p>
+                  <a style={{maxWidth: "360px", textAlign: "center"}}className="nav-link link-parceiro" href="/contact">Quero ser parceiro</a>
+                </div>
+                <div className="col-lg-6 py-3 py-lg-auto col-md-12 text-md-center">
+                  <div>
+                    <img className="mw-100" alt="slider" src="https://via.placeholder.com/535x400" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        <div style={{background: "#e6e9f3"}} className="bg-new py-5">
+          <div className='container'>
+          <h2 className='text-center'>Logística FULFILLMENT</h2>
+          <p className='text-center'>
+          Somos pioneiros na logística fulfillment
+          </p>
+          <div className="processo__grid mb-4">
+            <div className="grid__item p-4">
+              <div className="number">1</div>
+              <h3>Escolha do produto</h3>
+              <p>
+              Escolher o produto ideal faz toda a diferença. 
+              Nosso time entende com precisão a sua necessidade 
+              e ajuda a escolher o melhor para você.
+              </p>
+            </div>
+            <div className="grid__item p-4">
+              <div className="number">2</div>
+              <h3>Escolha do produto</h3>
+              <p>
+              Escolher o produto ideal faz toda a diferença. 
+              Nosso time entende com precisão a sua necessidade 
+              e ajuda a escolher o melhor para você.
+              </p>
+            </div>
+            <div className="grid__item p-4">
+              <div className="number">3</div>
+              <h3>Escolha do produto</h3>
+              <p>
+              Escolher o produto ideal faz toda a diferença. 
+              Nosso time entende com precisão a sua necessidade 
+              e ajuda a escolher o melhor para você.
+              </p>
+            </div>
+            <div className="grid__item p-4">
+              <div className="number">4</div>
+              <h3>Escolha do produto</h3>
+              <p>
+              Escolher o produto ideal faz toda a diferença. 
+              Nosso time entende com precisão a sua necessidade 
+              e ajuda a escolher o melhor para você.
+              </p>
+            </div>
+          </div>
+        </div>
+        </div>
+
+        <section className="bg-green">
+          <div className="container py-5 py-lg-0 d-flex flex-column flex-lg-row align-items-center justify-content-between">
+            <div className='anvisa col-md-6'>
+              <h2>Parcerias de sucesso</h2>
+              <p>
+              Acreditamos que não se faz nada grande sozinho, 
+              por isso, buscamos sempre parceiros que estejam 
+              alinhados a nossos princípios e valores, 
+              para assim, proporcionarmos aos nossos clientes 
+              uma melhor experiência e um serviço de excelência.
+              </p>
+            </div>
+            <div className='image d-none d-md-flex col-auto'>
+              <img style={{marginTop: "-25px"}} src="https://via.placeholder.com/355x400" alt="ooo" />
+            </div>
+          </div>
+        </section>
+
+        <div style={{background: "#e6e9f3"}} className="bg-new py-5">
+          <div className='container'>
+          <h2 className='text-center'>Logística FULFILLMENT</h2>
+          <p className='text-center'>
+          Somos pioneiros na logística fulfillment
+          </p>
+          <div className="processo__grid mb-4">
+            <div className="grid__item p-4">
+              <div className="number">1</div>
+              <h3>Escolha do produto</h3>
+              <p>
+              Escolher o produto ideal faz toda a diferença. 
+              Nosso time entende com precisão a sua necessidade 
+              e ajuda a escolher o melhor para você.
+              </p>
+            </div>
+            <div className="grid__item p-4">
+              <div className="number">2</div>
+              <h3>Escolha do produto</h3>
+              <p>
+              Escolher o produto ideal faz toda a diferença. 
+              Nosso time entende com precisão a sua necessidade 
+              e ajuda a escolher o melhor para você.
+              </p>
+            </div>
+            <div className="grid__item p-4">
+              <div className="number">3</div>
+              <h3>Escolha do produto</h3>
+              <p>
+              Escolher o produto ideal faz toda a diferença. 
+              Nosso time entende com precisão a sua necessidade 
+              e ajuda a escolher o melhor para você.
+              </p>
+            </div>
+            <div className="grid__item p-4">
+              <div className="number">4</div>
+              <h3>Escolha do produto</h3>
+              <p>
+              Escolher o produto ideal faz toda a diferença. 
+              Nosso time entende com precisão a sua necessidade 
+              e ajuda a escolher o melhor para você.
+              </p>
+            </div>
+          </div>
+        </div>
+        </div>
+
+        <section>
+          <div style={{gap: "4rem"}} className="container py-5 d-flex flex-lg-row flex-column align-items-center justify-content-between">
+            <div className="image">
+              <img src="https://via.placeholder.com/600x400" alt="video" />
+            </div>
+            <div style={{width: "60%", maxWidth: "565px"}} className='w-100 fiscal'>
+              <h2>Incentivo Fiscal</h2>
+              <p>
+              Focados no sucesso do seu negócio, 
+              temos a opção de inclusão da sua empresa no nosso 
+              sistema de incentivo fiscal único com redução de impostos, 
+              facilitando a gestão financeira da sua empresa.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className='bg-green'>
+          <div className="container align-items-center d-block d-md-flex">
+            <img className='mr-0 mr-lg-5' src="https://via.placeholder.com/620x400" alt="atendimento" />
+            <div className='sac-section'>
+              <h2>SAC Completo</h2>
+              <p>
+                Com o CareFee Kapsula, seu produto possui 
+                suporte ao seu cliente final, sem custo adicional. 
+                Atendimento personalizado para tirar dúvidas, 
+                obter informações e resolver o que precisar.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className='has-bg mt-4 position-relative' style={{padding: "4rem 1rem"}}>
+          <div className="container">
+            <div className='localizacao'>
+              <h2>Localização estratégica</h2>
+              <p>
+                O nosso centro de distribuição está 
+                localizado em Governador Valadares (MG), 
+                está situado no Vale do Rio Doce a cerca de 
+                320 km a leste da capital do estado. Todavia, 
+                possuímos parceiros estratégicos para 
+                ampliar a nossa rede de cobertura e 
+                realizarmos um envio rápido para todo o Brasil.
+              </p>
+            </div>
+          </div>
+        </section>
       </div>
     </section>
   )
