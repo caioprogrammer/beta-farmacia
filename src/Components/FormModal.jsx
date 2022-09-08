90f2c2a68c3f4dd1de8031b088d57bf8593a681c
Caio Nogueira <caio@fuerzastudio.com.br>
2022-09-08 16:03:31 -0300

    formulario adicionado

--- src/Components/FormModal.jsx
+++ src/Components/FormModal.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FormModal = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [assunto, setAssunto] = useState("");
+
+  const [textarea, setTextarea] = useState("");
+
+  const handleChange = (event) => {
+    setTextarea(event.target.value)
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name} | ${email} ${assunto}, ${textarea}`)
+  }
+
   return (
     <div className='modalForm'>
       <div className="modal fade" id="exampleModalCenter" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
@@ -13,22 +28,22 @@ const FormModal = () => {
               </button>
             </div>
             <div className="modal-body">
-              <form className="contact__form">
+              <form onSubmit={handleSubmit} className="contact__form">
                 <div className="">
                   <label htmlFor="name">Nome Completo</label>
-                  <input className="mw-100" type="text" id="name" name="name" placeholder="Digite seu nome" required="" value="" />
+                  <input value={name} onChange={(e) => setName(e.target.value)} className="mw-100" type="text" id="name" name="name" placeholder="Digite seu nome" required=""/>
                 </div>
                 <div className="">
                   <label htmlFor="email">Email</label>
-                  <input className="mw-100" type="email" id="email" name="email" placeholder=" Ex: seunome@gmail.com" required="" value=""/>
+                  <input value={email} onChange={(e) => setEmail(e.target.value)} className="mw-100" type="email" id="email" name="email" placeholder=" Ex: seunome@gmail.com" required=""/>
                 </div>
                 <div className="">
                   <label htmlFor="assunto">Assunto</label>
-                  <input className="mw-100" type="text" id="assunto" name="assunto" placeholder=" Digite o assunto" required="" value=""/>
+                  <input value={assunto} onChange={(e) => setAssunto(e.target.value)} className="mw-100" type="text" id="assunto" name="assunto" placeholder=" Digite o assunto" required=""/>
                 </div>
                 <div className="">
                   <label htmlFor="mensagem">Mensagem</label>
-                  <textarea className="mw-100" rows="3" id="mensagem" name="mensagem" placeholder=" Escreva sua mensagem..." required=""></textarea>
+                  <textarea value={textarea} onChange={handleChange} className="mw-100" rows="3" id="mensagem" name="mensagem" placeholder=" Escreva sua mensagem..." required=""></textarea>
                 </div>
                 <div>
                   <button className="mw-100" type="submit">Enviar Mensagem </button>
