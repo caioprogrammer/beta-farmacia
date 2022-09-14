e200f2c270c3ab4eeca4caa124f8cda4619cfc97
Caio Nogueira <caio@fuerzastudio.com.br>
2022-09-14 18:49:40 -0300

    teste

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,8 +2,8 @@ export { default as iconVelocidade } from "../assets/icon-velocidade.svg";
 export { default as iconFacilidade } from "../assets/icon-facilidade.svg";
 export { default as iconConfiavel } from "../assets/icon-confiavel.svg"
 export { default as iconSacCompleto } from "../assets/icon-sac-completo.svg";
-export { default as FrenteItalabs } from "../assets/frente_italabs_home.jpeg";
-export { default as Principal2 } from "../assets/imagem_2_principal.jpeg";
-export { default as Principal3 } from "../assets/imagem_3_principal.jpeg";
-export { default as Principal4 } from "../assets/imagem_4_principal.jpeg";
-export { default as Fabricacao1 } from "../assets/fabricacao_imagem1.jpeg";
+export { default as FrenteItalabs } from "../assets/frente_italabs_home.webp";
+export { default as Principal2 } from "../assets/imagem_2_principal.webp";
+export { default as Principal3 } from "../assets/imagem_3_principal.webp";
+export { default as Principal4 } from "../assets/imagem_4_principal.webp";
+export { default as Fabricacao1 } from "../assets/fabricacao_imagem1.webp";
